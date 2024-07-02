22ee51f12dd49402a53200a555c1e02afa7808ce
Sophie Colette <69583383+SC-Git1@users.noreply.github.com>
2024-07-02 13:07:16 +0200

    Update remove_empty_seqs.py

--- remove_polyA_only.py
+++ remove_polyA_only.py
@@ -4,25 +4,8 @@ from Bio import SeqIO
 import sys
 import os
 
-def removepolyAonlyKeepBoth(file):
-	record_iterator = SeqIO.parse(file, "fastq")
 
-	filename = os.path.basename(file)
-	folder = os.path.dirname(file)
-
-	filename_ok = "passed_" + filename
-	filename_notok = "removed_" +  filename
-
-	with open(filename_ok, "w") as out_ok:
-		with open(filename_notok, "w") as out_notok:
-
-			for record in record_iterator:
-				if len(str(record.seq).replace("A","")) != 0:
-					SeqIO.write(record, out_ok, 'fastq')
-				else:
-					SeqIO.write(record, out_notok, 'fastq')
-
-def removepolyAonly(infile, outfile):
+def removeEmptySequences(infile, outfile):
         record_iterator = SeqIO.parse(infile, "fastq")
 
         with open(outfile, "w") as out_ok:
@@ -34,4 +17,4 @@ def removepolyAonly(infile, outfile):
 if __name__ == "__main__":
 	infile = sys.argv[1]
 	outfile = sys.argv[2]
-	removepolyAonly(infile, outfile)
+	removeEmptySequences(infile, outfile)
